6bed5fe83d56508c2fc6531c0ee346af4972028a
yuqingzou <yisubai@icloud.com>
2019-09-22 23:42:21 -0400

    study note for 9/22

--- blackbox/bin/mongoConnector.js
+++ blackbox/bin/mongoConnector.js
@@ -1,44 +1,47 @@
 //Import the mongoose module
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 
 //Set up default mongoose connection
-var mongoDB = 'mongodb://127.0.0.1/my_database';
+var mongoDB = "mongodb://127.0.0.1/my_database";
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 
 //Get the default connection
 var db = mongoose.connection;
 
 //Bind connection to error event (to get notification of connection errors)
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 //Require Mongoose
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 
 //Define a schema
 var Schema = mongoose.Schema;
 
 //template for all genre
-var schema = new Schema(
-    {
-        name: String,
-        binary: Buffer,
-        living: Boolean,
-        updated: { type: Date, default: Date.now() },
-        age: { type: Number, min: 18, max: 65, required: true },
-        mixed: Schema.Types.Mixed,
-        _someId: Schema.Types.ObjectId,
-        array: [],
-        ofString: [String], // You can also have an array of each of the other types too.
-        nested: { stuff: { type: String, lowercase: true, trim: true } },
-        eggs: {
-            type: Number,
-            min: [6, 'Too few eggs'],
-            max: 12,
-            required: [true, 'Why no eggs?']
-        },
-        drink: {
-            type: String,
-            enum: ['Coffee', 'Tea', 'Water',]
-        }
-    });
-    })
+var schema = new Schema({
+  name: String,
+  binary: Buffer,
+  living: Boolean,
+  updated: { type: Date, default: Date.now() },
+  age: { type: Number, min: 18, max: 65, required: true },
+  mixed: Schema.Types.Mixed,
+  _someId: Schema.Types.ObjectId,
+  array: [],
+  ofString: [String], // You can also have an array of each of the other types too.
+  nested: { stuff: { type: String, lowercase: true, trim: true } },
+  eggs: {
+    type: Number,
+    min: [6, "Too few eggs"],
+    max: 12,
+    required: [true, "Why no eggs?"]
+  },
+  drink: {
+    type: String,
+    enum: ["Coffee", "Tea", "Water"]
+  }
+});
+// new instance and .save() remember callback function are receive erro first api so err should be the frist arg
+SomeModel.create({ name: "also_awesome" }, function(err, awesome_instance) {
+  if (err) return handleError(err);
+  // saved!
+});
